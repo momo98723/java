06dff009e2cd16f1a3246e303fb1c9c351843bfc
momo98723 <momo98723@naver.com>
2023-10-17 22:42:38 +0900

    2

---
+++ java_practice2/src/practice2.java
@@ -0,0 +1,18 @@
+
+public class practice2 {
+
+	public static void main(String[] args) {
+		int i=0;
+		int j=0;
+		while(i<=10) {
+			while(j<=i) {
+				System.out.print("*");
+				j++;
+		System.out.println();
+		i++;
+			}
+		}
+
+	}
+
+}
